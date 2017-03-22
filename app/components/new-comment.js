7329f6768b9bb0633066807800e92dc7bb94852c
Maggie Harrington and Stella Huayhuaca <stella@>
2017-03-22 14:56:48 -0700

    add one to many relationship model

--- app/components/new-comment.js
+++ app/components/new-comment.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewComment: false,
+  actions: {
+    commentFormShow() {
+      this.set('addNewComment', true);
+    },
+    saveComment() {
+     var params = {
+       author: this.get('author'),
+       title: this.get('title'),
+       body: this.get('body'),
+       post: this.get('post')
+     };
+     this.set('addNewComment', false);
+     this.sendAction('saveComment', params);
+   }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('post');
-  },
+  return Ember.RSVP.hash({
+    posts: this.store.findAll('post'),
+    comments: this.store.findAll('comment')
+  });
+},
   actions: {
     savePost(params) {
       var newPost = this.store.createRecord('post', params);
